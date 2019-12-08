1d506984803c6ba597105f989957d3d777cc5216
Daniel Barclay <barclaysd@me.com>
2019-12-08 15:38:01 +0000

    Refactor to abstract invoking of methods into MapService

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import MapService from './services/MapService';
 import User from './User';
+import Company from './Company';
 
 const user = new User();
+const company = new Company();
 
 const mapService = new MapService('map');
 
 mapService.display();
-mapService.addMarker(user.location, `${user.name}'s location`);
+mapService.displayMarker(
+  user.location,
+  user.name,
+  `<p>${user.name}'s current location</p>`,
+);
+mapService.displayMarker(
+  company.location,
+  company.name,
+  `<h1>${company.name} ${company.type}</h1><i>${company.slogan}</i>`,
+);
--- src/services/MapService.ts
+++ src/services/MapService.ts
@@ -1,9 +1,11 @@
 import { IPoint } from '../models/IPoint';
 import Marker = google.maps.Marker;
+import InfoWindow = google.maps.InfoWindow;
 
 export default class MapService {
   private zoomLevel = 3;
   private readonly googleMap: google.maps.Map;
+  private googleMapMarker: Marker;
 
   constructor(mapDivId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {
@@ -19,11 +21,30 @@ export default class MapService {
     return this.googleMap;
   }
 
-  public addMarker(position: IPoint, label: string): Marker {
-    return new google.maps.Marker({
+  private generateMarker(position: IPoint, label: string): Marker {
+    this.googleMapMarker = new google.maps.Marker({
       map: this.googleMap,
       position,
       label,
     });
+    return this.googleMapMarker;
+  }
+
+  private popupWindow(text: string): InfoWindow {
+    return new google.maps.InfoWindow({
+      content: `<div>${text}</div>`,
+    });
+  }
+
+  private popupWindowListener(popupWindow: InfoWindow, marker: Marker) {
+    google.maps.event.addListener(marker, 'click', () => {
+      popupWindow.open(this.googleMap, marker);
+    });
+  }
+
+  public displayMarker(position: IPoint, label: string, text: string) {
+    const marker = this.generateMarker(position, label);
+    const popupWindow = this.popupWindow(text);
+    this.popupWindowListener(popupWindow, marker);
   }
 }
